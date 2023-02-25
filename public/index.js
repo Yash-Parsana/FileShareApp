c23ad7c529433f53cfe969c13da1baeb2d0cdd69
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2023-02-25 15:24:18 +0530

    Domain name change

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://localhost:3000";
+const baseURL = "https://filesharingapp-5kmg.onrender.com";
 const uploadURL = `${baseURL}/api/files/upload`;
 const emailURL = `${baseURL}/api/files/sendmail`;
 
